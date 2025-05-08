5a46bd0319afaed0c7688387c9ab1682d428eeb0
Iago Rivas <iagorivas@gmail.com>
2025-05-08 22:34:57 +0200

    clasepractica_ejercicios-semana1

---
+++ curso2-avanzado/Ejercicios/Semana1/dia1/app/src/Ejercicio2/Exepciones.java
@@ -0,0 +1,38 @@
+package Ejercicio1;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Exepciones {
+
+    public String validarNoVacio (String dato) {
+
+        String datoCorrecto = "";
+
+
+            if (!dato.isEmpty()) {
+                 datoCorrecto = dato;
+            }
+            else
+            {
+                System.err.println("no puede ir vacio");
+            }
+
+
+
+      return datoCorrecto;
+    }
+    public Date validarFecha(String fechaTexto) {
+
+        Date fechaValida = null;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            fechaValida = sdf.parse(fechaTexto);
+
+        } catch (ParseException e) {
+            System.err.println("La fecha es incorrecta");
+        }
+       return fechaValida;
+    }
+}
