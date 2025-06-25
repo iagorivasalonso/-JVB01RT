04a28d9a42a1b108572dce3ec8c30c789a3811da
Iago Rivas <iagorivas@gmail.com>
2025-06-25 13:27:19 +0200

    limp_code

--- curso3-spring-boot/Ejercicios/Semana1/dia4/app/src/main/java/com/hackaboss/app/AppApplication.java
+++ curso3-spring-boot/Ejercicios/Semana1/dia4/app/src/main/java/com/hackaboss/app/AppApplication.java
@@ -12,12 +12,3 @@ public class AppApplication {
 
 }
 
-/*
-eventos.add(new evento(1L, "fiestaVerano", "ddd", LocalDate.of(2025, 6, 30), LocalTime.of(20, 30)));
-eventos.add(new evento(2L, "conciertoRock", "música en vivo", LocalDate.of(2025, 7, 10), LocalTime.of(19, 0)));
-eventos.add(new evento(3L, "feriaGastronomica", "comida internacional", LocalDate.of(2025, 8, 5), LocalTime.of(12, 0)));
-eventos.add(new evento(4L, "maratonCiudad", "evento deportivo", LocalDate.of(2025, 9, 15), LocalTime.of(8, 30)));
-eventos.add(new evento(5L, "nocheCine", "proyección al aire libre", LocalDate.of(2025, 7, 25), LocalTime.of(21, 15)));
-
-
- */
