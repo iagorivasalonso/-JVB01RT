18dda5589b1bc7ddd9728e00caa6b9fe6dcb2dc0
Iago Rivas <iagorivas@gmail.com>
2025-06-26 16:07:13 +0200

    incorporeDTO

--- curso3-spring-boot/Ejercicios/Semana2/dia6/app/src/main/java/com/hackaboss/app/controllers/InstitutoController.java
+++ curso3-spring-boot/Ejercicios/Semana2/dia6/app/src/main/java/com/hackaboss/app/controllers/InstitutoController.java
@@ -1,4 +1,34 @@
 package com.hackaboss.app.controllers;
 
-public class institutoController {
+import com.hackaboss.app.dtos.CalificacionesDTO;
+import com.hackaboss.app.models.Estudiante;
+import com.hackaboss.app.services.EstudianteCalificacionServicio;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class InstitutoController {
+
+    EstudianteCalificacionServicio institutoServicio = new EstudianteCalificacionServicio();
+
+    //http://localhost:8080/estudiantes
+    @GetMapping("/estudiantes")
+    public List<Estudiante> obtenerTodos() {
+        return institutoServicio.listar();
+    }
+
+    //http://localhost:8080/calificaciones/1001
+    @GetMapping("/calificaciones/{nMatricula}")
+    public double mediaEstudiante(@PathVariable int nMatricula) {
+
+        return institutoServicio.mediaEstudiante(nMatricula);
+    }
+    @GetMapping("/calificaciones/ordenDesc")
+    public List<CalificacionesDTO> califEstudiante() {
+
+        return institutoServicio.notasEstudiantes();
+    }
+
 }
--- curso3-spring-boot/Ejercicios/Semana2/dia6/app/src/main/java/com/hackaboss/app/models/Calificaciones.java
+++ curso3-spring-boot/Ejercicios/Semana2/dia6/app/src/main/java/com/hackaboss/app/models/Calificaciones.java
@@ -1,4 +1,18 @@
 package com.hackaboss.app.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Calificaciones {
+
+     private List<Double> calificaciones;
+     private double promedio;
+     private Estudiante estudiante;
+
 }
