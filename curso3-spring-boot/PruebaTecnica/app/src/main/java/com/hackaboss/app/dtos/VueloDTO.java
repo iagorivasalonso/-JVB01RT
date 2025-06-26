a326130f84c66fea1181a5c4d90ba084b66d542a
Iago Rivas <iagorivas@gmail.com>
2025-06-26 21:28:59 +0200

    createDTO

---
+++ curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/dtos/VueloDTO.java
@@ -0,0 +1,30 @@
+package com.hackaboss.app.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class VueloDTO {
+
+    @JsonProperty("publicID")
+    private int id;
+    @JsonProperty("publicNombreVuelo")
+    private String nombreVuelo;
+    @JsonProperty("publicEmpresa")
+    private String empresa;
+    @JsonProperty("publicLugarSalida")
+    private String lugarSalida;
+    @JsonProperty("publicLugarLlegada")
+    private String lugarLlegada;
+    @JsonProperty("publicFechaSalida")
+    private LocalDate fechaSalida;
+    @JsonProperty("publicFechaLlegada")
+    private LocalDate fechaLlegada;
+}
