ee3e1175f26d217ab42dcfe93629bb3c77fa2b62
Iago Rivas <iagorivas@gmail.com>
2025-06-26 20:02:28 +0200

    createEntidad

---
+++ curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/models/Vuelo.java
@@ -0,0 +1,24 @@
+package com.hackaboss.app.models;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Vuelo {
+
+    private int id;
+    private String nombreVuelo;
+    private String empresa;
+    private String lugarSalida;
+    private String lugarLlegada;
+    private LocalDate fechaSalida;
+    private LocalDate fechaLlegada;
+
+}
+
+
