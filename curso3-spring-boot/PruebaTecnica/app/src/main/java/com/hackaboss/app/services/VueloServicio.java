492a736852704912b9bf2ae2ccf5dcd1c1024439
Iago Rivas <iagorivas@gmail.com>
2025-06-30 11:39:47 +0200

    modificar_datos_defecto

--- curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/services/VueloServicio.java
+++ curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/services/VueloServicio.java
@@ -21,9 +21,9 @@ public class VueloServicio implements IVueloServicio {
         this.vuelos = new ArrayList<>();                      //carga de datos
         vuelos.add(new Vuelo(1, "H001-V", "Iberia", "Madrid", "Buenos Aires", LocalDate.of(2025, 3, 10), LocalDate.of(2025, 3, 11)));
         vuelos.add(new Vuelo(2, "H002-V", "Air France", "París", "Ciudad de México", LocalDate.of(2025, 4, 5), LocalDate.of(2025, 4, 6)));
-        vuelos.add(new Vuelo(3, "H003-V", "Lufthansa", "Berlín", "Nueva York", LocalDate.of(2025, 5, 12), LocalDate.of(2025, 5, 12)));
+        vuelos.add(new Vuelo(3, "H003-V", "Iberia", "Berlín", "Nueva York", LocalDate.of(2025, 5, 12), LocalDate.of(2025, 5, 12)));
         vuelos.add(new Vuelo(4, "H004-V", "American Airlines", "Miami", "Lima", LocalDate.of(2025, 6, 20), LocalDate.of(2025, 6, 21)));
-        vuelos.add(new Vuelo(5, "H005-V", "LATAM", "Santiago", "São Paulo", LocalDate.of(2025, 7, 1), LocalDate.of(2025, 7, 1)));
+        vuelos.add(new Vuelo(5, "H005-V", "LATAM", "Santiago", "Buenos Aires", LocalDate.of(2025, 7, 1), LocalDate.of(2025, 7, 1)));
     }
 
     @Override
