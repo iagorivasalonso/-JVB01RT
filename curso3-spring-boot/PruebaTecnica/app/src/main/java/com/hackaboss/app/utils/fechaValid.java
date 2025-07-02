f538eef33f4b44032dbb0c53cff4f40b9f61e3af
Iago Rivas <iagorivas@gmail.com>
2025-07-02 11:01:10 +0200

    mensajeErrorFecha

--- curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/utils/fechaValid.java
+++ curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/utils/fechaValid.java
@@ -7,7 +7,6 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.LocalDate;
-import java.util.List;
 
 
 @ControllerAdvice
