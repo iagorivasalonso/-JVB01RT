f027ac0d18dd18a3b4d3cc4d0aba491fea5689b9
Iago Rivas <iagorivas@gmail.com>
2025-07-02 13:14:59 +0200

    añadir_exception_campos_null

--- curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/utils/fechaValid.java
+++ curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/utils/fechaValid.java
@@ -4,6 +4,7 @@ import com.hackaboss.app.response.VueloRespuesta;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.LocalDate;
@@ -20,6 +21,24 @@ public class fechaValid {
 
         VueloRespuesta vRespuesta =  new VueloRespuesta(
                 "Error: "+capError[1]+", no tiene formato de fecha",
+                HttpStatus.NOT_FOUND.value(),
+                LocalDate.now());
+
+        return ResponseEntity.badRequest().body(vRespuesta);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> manejarErroresValidacion(MethodArgumentNotValidException ex) {
+
+        String [] capError = ex.getMessage().split("message");
+        int ultimaposicion = capError.length-1;
+
+        String error0=capError[ultimaposicion];
+        String error= error0.substring(2,error0.length()-3);
+        System.err.println(error);
+
+        VueloRespuesta vRespuesta =  new VueloRespuesta(
+                error,
                 HttpStatus.NOT_FOUND.value(),
                 LocalDate.now());
 
