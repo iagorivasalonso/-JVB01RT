22598dba18e444f67bad8c471513f2a6348ab814
Iago Rivas <iagorivas@gmail.com>
2025-07-02 10:31:44 +0200

    fechaLLegada

--- curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/dtos/VueloDTO.java
+++ curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/dtos/VueloDTO.java
@@ -28,11 +28,9 @@ public class VueloDTO {
     private String lugarLlegada;
     @JsonProperty("publicFechaSalida")
     @NotNull(message = "La fecha de salida es obligatoria")
-    @FutureOrPresent(message = "La fecha de llegada debe ser hoy o futura")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaSalida;
     @JsonProperty("publicFechaLlegada")
-    @FutureOrPresent(message = "La fecha de llegada debe ser hoy o futura")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaLlegada;
 }
