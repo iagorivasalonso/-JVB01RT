d2daeb57c3a6cacf1715720bc5f3f89d201d321d
Iago Rivas <iagorivas@gmail.com>
2025-07-02 11:00:43 +0200

    mensajeErrorFecha

--- curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/utils/fechaValid.java
+++ curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/utils/fechaValid.java
@@ -6,19 +6,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+import java.util.List;
+
 
 @ControllerAdvice
 public class fechaValid {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity  manejarErroresValidacion (HttpMessageNotReadableException ex){
-        //System.err.println(ex.getMessage());
+
+
+        String [] capError = ex.getMessage().split("\\'");
 
         VueloRespuesta vRespuesta =  new VueloRespuesta(
-                "La fecha es incorrecta (" + ex.getMessage() + ")" ,
+                "Error: "+capError[1]+", no tiene formato de fecha",
                 HttpStatus.NOT_FOUND.value(),
                 LocalDate.now());
 
