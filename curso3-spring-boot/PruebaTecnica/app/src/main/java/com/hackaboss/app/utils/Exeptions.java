3536af4c4abd7ee3480fdb8c8ae64e057f9fb25c
Iago Rivas <iagorivas@gmail.com>
2025-07-02 14:45:13 +0200

    comentar_codigo

--- curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/utils/Exeptions.java
+++ curso3-spring-boot/PruebaTecnica/app/src/main/java/com/hackaboss/app/utils/Exeptions.java
@@ -13,13 +13,13 @@ import java.time.LocalDate;
 @ControllerAdvice
 public class Exeptions {
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity  manejarErroresValidacion (HttpMessageNotReadableException ex){
+    @ExceptionHandler(HttpMessageNotReadableException.class)  //comprobarFecha valida
+    public ResponseEntity  manejarErroresValidacionFecha (HttpMessageNotReadableException ex){
 
 
-        String [] capError = ex.getMessage().split("\\'");
+        String [] capError = ex.getMessage().split("\\'");  // para quedarme con lo que interesa
 
-        VueloRespuesta vRespuesta =  new VueloRespuesta(
+        VueloRespuesta vRespuesta =  new VueloRespuesta(  //imprimir respuesta
                 "Error: "+capError[1]+", no tiene formato de fecha",
                 HttpStatus.NOT_FOUND.value(),
                 LocalDate.now());
@@ -30,14 +30,14 @@ public class Exeptions {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> manejarErroresValidacion(MethodArgumentNotValidException ex) {
 
-        String [] capError = ex.getMessage().split("message");
+        String [] capError = ex.getMessage().split("message"); // para quedarme con lo que interesa
         int ultimaposicion = capError.length-1;
 
         String error0=capError[ultimaposicion];
         String error= error0.substring(2,error0.length()-3);
-        System.err.println(error);
 
-        VueloRespuesta vRespuesta =  new VueloRespuesta(
+
+        VueloRespuesta vRespuesta =  new VueloRespuesta( //imprimir respuesta
                 error,
                 HttpStatus.NOT_FOUND.value(),
                 LocalDate.now());
