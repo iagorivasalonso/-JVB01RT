e55aa3b4d350c1f14f6c55d596ebda1c99ff71f3
Iago Rivas <iagorivas@gmail.com>
2025-07-07 13:57:40 +0200

    mapeo_to_dto

--- curso3-spring-boot/Ejercicios/1-jpa-hibernate/app/src/main/java/com/hackaboss/app/services/implementaciones/TemaServicio.java
+++ curso3-spring-boot/Ejercicios/1-jpa-hibernate/app/src/main/java/com/hackaboss/app/services/implementaciones/TemaServicio.java
@@ -3,7 +3,9 @@ package com.hackaboss.app.services.implementaciones;
 import com.hackaboss.app.dtos.TemaDTO;
 import com.hackaboss.app.models.Tema;
 import com.hackaboss.app.services.interfaces.ITemaServicio;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TemaServicio implements ITemaServicio {
     @Override
     public TemaDTO crearTema() {
@@ -17,7 +19,7 @@ public class TemaServicio implements ITemaServicio {
 
     @Override
     public TemaDTO mapeoToDTO(Tema t) {
-        return null;
+        return new TemaDTO(t.getId_tema(),t.getNombre(),t.getDescripcion());
     }
 
     @Override
